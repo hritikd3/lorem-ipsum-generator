44eb44690c440870e4f6212753571f3198e80e76
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-09 23:53:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./styles.css";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+      alert(" Ayyo ! you know that you wrote negative input 😵");
+    }
+    if (count > 8) {
+      amount = 8;
+      alert(
+        " Ayyo ! you are demanding very much paragraph in one go, kindly take little litlle 😀"
+      );
+    }
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
